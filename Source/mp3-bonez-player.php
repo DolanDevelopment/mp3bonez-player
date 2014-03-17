49bcd5e9afb8f36973348c3520ca35cdc384bbeb
Ryan Dolan <ryanddolan@hotmail.com>
2014-03-17 12:57:55 -0500

    Version 1.0: See README for Wordpress Plugin and Shortcode related instructions

---
+++ Source/mp3-bonez-player.php
@@ -0,0 +1,135 @@
+<?php
+	/**
+	 * Plugin Name: Mp3 Bonez Player
+	 * Plugin URI: http://github.com
+	 * Description: mp3 and wave player based on Wavesurfer.js
+	 * Version: 1.0
+	 * Author: Ryan Dolan
+	 * Author URI: http://rdd-soft.com
+	 * License: GPL2
+	 */
+
+	/*  Copyright 2014  Ryan Dolan  (email : ryanddolan@hotmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	*/
+
+	function initialize() {
+
+		if (function_exists('wp_enqueue_style')) {
+			wp_enqueue_style('mp3bonez_style', get_bloginfo('wpurl') . '/wp-content/plugins/mp3-bonez-player/style.css');
+		}
+
+
+		if (function_exists('wp_enqueue_script')) {
+
+			// for new libraries
+			wp_register_script('jquery_1_11_0', get_bloginfo('wpurl') . '/wp-content/plugins/mp3-bonez-player/vendor/jquery-1.11.0.min.js');
+			wp_register_script('wavesurfer', get_bloginfo('wpurl') . '/wp-content/plugins/mp3-bonez-player/vendor/wavesurfer.js');
+			wp_register_script('wavesurfer_drawer', get_bloginfo('wpurl') . '/wp-content/plugins/mp3-bonez-player/vendor/drawer.js');
+			wp_register_script('wavesurfer_drawer_canvas', get_bloginfo('wpurl') . '/wp-content/plugins/mp3-bonez-player/vendor/drawer.canvas.js');
+			wp_register_script('webaudio', get_bloginfo('wpurl') . '/wp-content/plugins/mp3-bonez-player/vendor/webaudio.js');
+			wp_register_script('mp3_bonez', get_bloginfo('wpurl') . '/wp-content/plugins/mp3-bonez-player/mp3bonez.js');
+			
+			wp_enqueue_script('jquery_1_11_0');
+			wp_enqueue_script('wavesurfer');
+			wp_enqueue_script('wavesurfer_drawer');
+			wp_enqueue_script('wavesurfer_drawer_canvas');
+			wp_enqueue_script('webaudio');
+			wp_enqueue_script('mp3_bonez');
+		}
+	}
+
+	//http://guid.us/GUID/PHP (modified to remove curly brackets)
+	function getGUID(){
+	    if (function_exists('com_create_guid')){
+	        return com_create_guid();
+	    }else{
+	        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+	        $charid = strtoupper(md5(uniqid(rand(), true)));
+	        $hyphen = chr(45);// "-"
+	        $uuid = substr($charid, 0, 8).$hyphen
+	            .substr($charid, 8, 4).$hyphen
+	            .substr($charid,12, 4).$hyphen
+	            .substr($charid,16, 4).$hyphen
+	            .substr($charid,20,12);	            
+	        return $uuid;
+	    }
+	}	
+
+	function create_player($attrs){
+
+		// get height and width information and use on inline styles
+		extract( shortcode_atts( array(
+				'src' => '',
+				'height' => 72,
+				'width' => 500,
+				'src_relative_to_wp_content' => 'false',
+				'wave_color' => '#CDCDCD',
+				'progress_color' => '#474747',
+				'responsive' => 'false'
+		), $attrs ) );
+
+		$play = get_bloginfo('wpurl') . "/wp-content/plugins/mp3-bonez-player/content/play-white.png";
+		$stop = get_bloginfo('wpurl') . "/wp-content/plugins/mp3-bonez-player/content/stop-white.png";
+
+		$containerHeight = $height . 'px';
+		$containerWidth = $width . 'px';
+		$halfContainerWidth = ($width / 2) . 'px';
+		$buttonContainerWidth = ($width + 2) . 'px';
+		$halfContainerHeight = ($height / 2) . 'px';
+
+		$loadingTop = ($halfContainerHeight - 10) . 'px';
+		$loadingLeft = ($halfContainerWidth - 50) . 'px';
+		$pluginDir = get_bloginfo('wpurl') . "/wp-content/plugins/mp3-bonez-player/";
+		$srcPath = '';
+		if ($src_relative_to_wp_content === 'true')
+			$srcPath = 	get_bloginfo('wpurl') . "/wp-content/" . $src;
+		else
+			$srcPath = $src;
+
+		$fileName = basename($srcPath);
+		$guid = getGUID();
+
+		return "
+			<div id='{$guid}' src='{$srcPath}' style='width: {$containerWidth};' class='mp3-bonez'>
+				<div class='wave'></div>
+				<div class='loading' style='top: {$loadingTop}; left: {$loadingLeft}'>Loading...</div>
+				<div class='mask' style='height: {$halfContainerHeight}; top: {$halfContainerHeight}; width:{$containerWidth};'></div>
+				<div class='time'></div>
+				<div class='button-container' style='width: {$buttonContainerWidth};'>
+					<div class='button'><img src='{$play}' /></div>
+					<div class='button'><img src='{$stop}' /></div>
+					<a href='{$srcPath}' type='application/octet-stream' download='{$fileName}'>download</a>
+				</div>
+				<div class='clear'></div>
+			</div>		
+			<script type='text/javascript'>
+				(function(){
+					var element = document.getElementById('{$guid}');
+					var waveElement = $(element).find('.wave').get()[0];
+					var timeElement = $(element).find('.time').get()[0];
+					var maskElement = $(element).find('.mask').get()[0];
+					var loadingElement = $(element).find('.loading').get()[0];
+					var responsive = {$responsive} ? true : false;
+					window.mp3bonez.createPlayer(element, waveElement, timeElement, maskElement, loadingElement, {$width}, {$height}, '{$pluginDir}', '{$wave_color}', '{$progress_color}', responsive);
+				})();
+			</script>
+		";
+	}
+
+	add_action('init', 'initialize');
+
+	add_shortcode('mp3bonez', 'create_player');
+?>
---
+++ Source/mp3bonez.js
@@ -0,0 +1,113 @@
+window.mp3bonez = (function(){
+
+	return {
+
+		createPlayer: function (
+						element, 
+						waveElement, 
+						timeElement, 
+						maskElement,
+						loadingElement, 
+						width, 
+						height, 
+						pluginDir, 
+						waveColor, 
+						progressColor, 
+						responsizeLayout) {
+
+			var wavesurfer = Object.create(WaveSurfer);
+			var playing = false;
+
+			var playPauseButton = $('#' + element.id + '.mp3-bonez .button:first-child');
+			var stopButton = $('#' + element.id + '.mp3-bonez .button:nth-child(2)');
+
+			wavesurfer.init({
+			    container: waveElement, // first child of the container
+			    waveColor: waveColor || '#CDCDCD',
+			    progressColor: progressColor || '#474747',
+			    dragSelection : false,
+		    	loopSelection : false,
+		    	height: height,
+		    	cursorWidth: 0
+			});
+
+			function stopPlayer(){
+				playing = false;
+				wavesurfer.stop();
+
+				playPauseButton.find('img').attr('src', pluginDir + 'content/play-white.png');	
+			}
+			function pausePlayer(){
+				playing = false;
+				wavesurfer.pause();
+
+				playPauseButton.find('img').attr('src', pluginDir + 'content/play-white.png');				
+			}
+			function startPlayer(){
+		    	playing = true;
+		    	wavesurfer.play();
+
+		    	playPauseButton.find('img').attr('src', pluginDir + 'content/pause-white.png');
+			}
+			function resizePlayer(waveLoaded){
+				var width = $(element.parentElement).width();
+				$(element).width(width);
+				$(element).find('.mask').width(width + 2);
+				$(element).find('.button-container').width(width + 2);
+				$(loadingElement).css('left', ((width / 2) - 50).toString() + 'px');
+				wavesurfer.drawer.containerWidth = wavesurfer.drawer.container.clientWidth;
+				if (waveLoaded)
+					wavesurfer.drawBuffer();
+			}
+
+			playPauseButton.on('click', function () {
+				if (!playing)
+					startPlayer(this);
+
+				else 
+					pausePlayer(this);
+			});
+			stopButton.on('click', function () {
+				stopPlayer();
+			});		
+
+
+			// play progress
+			wavesurfer.on('progress', function(progress){
+				var time = wavesurfer.getCurrentTime();
+				var duration = wavesurfer.getDuration();
+
+				timeElement.innerHTML = '<p>' + time.toFixed(2) + ':' + duration.toFixed(2) + '</p>';
+			});
+
+			// wave source ready
+			wavesurfer.on('ready', function(result){
+				timeElement.innerHTML = '<p>0.00:' + wavesurfer.getDuration().toFixed(2) + '</p>';
+
+				$(timeElement).css('opacity', 0.5);
+				$(loadingElement).css('display', 'none');
+				$(element).css('opacity', 1);
+
+				if (responsizeLayout)
+					resizePlayer(true);				
+			}); 
+
+			// finished
+			wavesurfer.on('finish', function(){
+				stopPlayer();
+			});
+
+			// responsive layout
+			if (responsizeLayout){
+				$(window).on('resize', function(){
+					resizePlayer(true);
+				});
+			}
+
+			$(timeElement).css('opacity', 0);
+			wavesurfer.load(element.getAttribute('src'));
+			if (responsizeLayout)
+				resizePlayer(false);
+		}
+	}
+})();
